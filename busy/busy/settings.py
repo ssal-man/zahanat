a447bf8fbcf572d58c4fb2369ef6e62db4f36a5c
ssal-man <52075034+ssal-man@users.noreply.github.com>
2019-11-16 20:47:18 +0530

    Update settings.py

--- busy/busy/settings.py
+++ busy/busy/settings.py
@@ -130,11 +130,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 CSRF_COOKIE_SECURE = False
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),    
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
         
-    ],
-    'PAGE_SIZE': 5
+    ]
 }
