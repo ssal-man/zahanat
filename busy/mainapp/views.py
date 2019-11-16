acd9366e301c8f5407a4f1ba94a91bcf243a1c68
Srijan <srijans38@gmail.com>
2019-11-16 14:22:51 +0530

    Added Views

--- busy/mainapp/views.py
+++ busy/mainapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Bus
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+def index(request):
+    context = {'buses' : Bus.objects.all()}
+    return(render(request, 'mainapp/base.html', context))
+
+@csrf_exempt
+def buslist(request):
+    if request.method == "GET": 
+        context = {'buses' : Bus.objects.all()}
+        return(render(request, 'mainapp/base.html', context))
+    elif request.method == "POST":
+        context = {'test' : 'This is post request'}
+        return(render(request, 'mainapp/base.html', context))
+    
